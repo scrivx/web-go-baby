6d7e9f2d1627c3a5cfafe2d589e090db09f1cde1
c4rlo5Riv3ra <jcriveraguadalupe@gmail.com>
2025-07-27 00:54:05 -0500

    fixed: main.go update and fuctional

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,29 @@
 package main
 
-import "github.com/scrivx/go-api-web/cmd/web/routes/mainRoute"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+
+	"github.com/scrivx/go-api-web/cmd/web/routes"
+	"github.com/scrivx/go-api-web/cmd/web/routes/mainRoute"
+)
 
 func InitRoutes() {
 	mainRoute.MainRender()
+	mainRoute.ClockRender()
 }
 
 func main() {
+	InitRoutes()
+	mux := routes.GetMuxInstance()
+	fileServer := routes.GetFileServerInstance()
+
+	mux.Handle("GET /static/", http.StripPrefix("/static/", fileServer))
+
+	slog.Log(context.TODO(), slog.LevelInfo, "🚀 Starting server on http://localhost:4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 	
 }
