58e533968c35b98a8e051919ec529f59f17b09f9
c4rlo5Riv3ra <jcriveraguadalupe@gmail.com>
2025-07-27 23:49:46 -0500

    Fixed: using embed

--- cmd/web/domain/main/main.domain.go
+++ cmd/web/domain/main/main.domain.go
@@ -5,15 +5,16 @@ import (
 
 	"github.com/scrivx/go-api-web/internal/models"
 	"github.com/scrivx/go-api-web/internal/utils"
+	"github.com/scrivx/go-api-web/ui"
 )
 
 func MainView(w http.ResponseWriter, r *http.Request) {
 	utils.CheckIfPath(w, r, models.RoutesInstance.MAIN)
 	files := []string{
-		"ui/html/base.html",
-		"ui/html/pages/main/main.html",
+		"html/base.html",
+		"html/pages/main/main.html",
 	}
 	
-	utils.ParseTemplateFiles(w, "base", utils.EmplyStruct, utils.EmplyFuncMap, files...)
+	utils.ParseTemplateFiles(w, "base", utils.EmplyStruct, utils.EmplyFuncMap, ui.Content, files...)
 }
 
